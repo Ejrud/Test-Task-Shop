74e9c436e6ec0c4c41a2c1ec8debf4e1cbc4ba81
Ejrud <den.maltsev1@gmail.com>
2023-07-21 23:40:59 +0400

    Alt currency button

---
+++ Assets/Project/Scripts/Systems/Core/Components/CurrencyButton.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+
+[System.Serializable]
+public class CurrencyButton
+{
+    public TMP_Text text => _text;
+    public Button button => _button;
+    public CurrencyType currencyType => _currencyType;
+
+    [SerializeField] private TMP_Text _text;
+    [SerializeField] private Button _button;
+    [SerializeField] private CurrencyType _currencyType;
+
+    public void SetButtonText(string text)
+    {
+        _text.text = text;
+    }
+}
--- Assets/Project/Scripts/View/ShopItemFrame.cs
+++ Assets/Project/Scripts/View/ShopItemFrame.cs
@@ -1,23 +1,25 @@
 using System;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ShopItemFrame : ItemFrame
 {
     public event Action<Item, CurrencyType> OnSelect;
-    [SerializeField] private Button _buyButton;
-    [SerializeField] private CurrencyType _currencyType;
-    
+    [SerializeField] private CurrencyButton[] _currencyButton;
+
     public override void Initialize(Item item, Sprite icon)
     {
         _item = item;
         _name.text = item.name;
         _icon.sprite = icon;
-        
-        _buyButton.onClick.AddListener(() =>
+
+        foreach (var unit in _currencyButton)
         {
-            OnSelect?.Invoke(_item, _currencyType);
-        });
+            unit.SetButtonText($"Buy for {unit.currencyType}"); 
+            unit.button.onClick.AddListener(() =>
+            {
+                OnSelect?.Invoke(_item, unit.currencyType);
+            });
+        }
     }
 
     public override void SetActive(bool active)
